4834ffc3cd5407621c139a0ae479db8e855c8e7c
Kaio Meireles <145077870+Kaiomeireles@users.noreply.github.com>
2023-11-23 15:09:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var header = document.querySelector("header");
+    var nav = document.querySelector("nav");
+  
+    window.addEventListener("scroll", function () {
+      var scrollPosition = window.scrollY;
+  
+      if (scrollPosition > 100) {
+        header.classList.add("scrolled");
+        nav.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+        nav.classList.remove("scrolled");
+      }
+    });
+  });
+  
 
